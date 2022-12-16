07fda45eeb965302faf432c0973402854adce8b3
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-12-16 13:28:30 +0300

    Add wrapper in the nav-mobile-menu

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -1,9 +1,11 @@
 const toggleMenu = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.nav__list');
 const logoMenu = document.querySelector('.logo__icon');
+const headerWrapper = document.querySelector('.header__wrapper');
 
 mobileMenu.classList.add('nav__list--close');
 toggleMenu.classList.add('menu-toggle--no-js');
+headerWrapper.classList.remove('header__wrapper--no-js');
 
 function openMobMenu() {
   toggleMenu.addEventListener('click', function () {
@@ -11,7 +13,9 @@ function openMobMenu() {
       toggleMenu.classList.remove('open');
       mobileMenu.classList.remove('nav__list--open');
       logoMenu.classList.remove('logo__icon--open');
+      headerWrapper.classList.remove('header__wrapper--open');
     } else {
+      headerWrapper.classList.add('header__wrapper--open');
       toggleMenu.classList.add('open');
       toggleMenu.classList.add('black');
       mobileMenu.classList.add('nav__list--open');
