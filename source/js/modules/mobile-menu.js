9a47ebb07d990214416975d8365b7dafc6963edb
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-12-16 15:12:58 +0300

    fix images and button

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -2,10 +2,30 @@ const toggleMenu = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.nav__list');
 const logoMenu = document.querySelector('.logo__icon');
 const headerWrapper = document.querySelector('.header__wrapper');
+const navItem = document.querySelectorAll('.nav__item');
+
 
 mobileMenu.classList.add('nav__list--close');
 toggleMenu.classList.add('menu-toggle--no-js');
 headerWrapper.classList.remove('header__wrapper--no-js');
+
+function controlsScroll() {
+  navItem.forEach((item) => {
+    item.addEventListener('click', function () {
+      if (item) {
+        document.body.style.overflow = '';
+        mobileMenu.classList.remove('nav__list--open');
+        logoMenu.classList.remove('logo__icon--open');
+        headerWrapper.classList.remove('header__wrapper--open');
+        toggleMenu.classList.remove('black');
+        toggleMenu.classList.remove('open');
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    });
+  });
+}
+
 
 function openMobMenu() {
   toggleMenu.addEventListener('click', function () {
@@ -14,12 +34,15 @@ function openMobMenu() {
       mobileMenu.classList.remove('nav__list--open');
       logoMenu.classList.remove('logo__icon--open');
       headerWrapper.classList.remove('header__wrapper--open');
+      document.body.style.overflow = '';
     } else {
       headerWrapper.classList.add('header__wrapper--open');
       toggleMenu.classList.add('open');
       toggleMenu.classList.add('black');
       mobileMenu.classList.add('nav__list--open');
       logoMenu.classList.add('logo__icon--open');
+      controlsScroll();
+      document.body.style.overflow = 'hidden';
     }
   });
 }
